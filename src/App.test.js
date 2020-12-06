e1b7a7d387348ed928a2643847f0235edeb276ec
Will Lorand <will@milemarker3.org>
2020-12-06 07:12:38 -0800

    cra cleanup and added google font

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders nivo bar chart heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/nivo bar chart/i);
+  expect(headingElement).toBeInTheDocument();
 });
